7eddaef46f3341c0bcc427b3d2554517ec3cd44f
DiegoBV <diego.baratto8@gmail.com>
2018-02-26 21:01:07 +0100

    Exit

--- ProyectosSDL/HolaSDL/Scene.cpp
+++ ProyectosSDL/HolaSDL/Scene.cpp
@@ -1,5 +1,7 @@
 #include "Scene.h"
-
+#include "SDLApp.h"
+#include "ItemInventario.h"
+#include "Resources.h"
 
 
 Scene::Scene()
@@ -8,6 +10,18 @@ Scene::Scene()
 	//Idealmente lee de un archivo
 }
 
+Scene::Scene(bool kk, SDLApp* app, int SceneNum) { //pruebas solo, Leo no me mates T_T
+	app = app;
+	SceneNum = SceneNum;
+	if (kk) { 
+		SceneItems.push_back(new ItemInventario(app, 368, 0, 64, 64, "descripcion", "tag", app->getResources()->getImageTexture(Resources::ImagenTest)));
+		SceneItems.push_back(new ItemInventario(app, 100, 0, 64, 64, "dsdwreeion", "teweg", app->getResources()->getImageTexture(Resources::ImagenTest)));
+		SceneItems.push_back(new ItemInventario(app, 668, 0, 64, 64, "dffffn", "ffftag", app->getResources()->getImageTexture(Resources::ImagenTest)));
+	}
+	else {
+		SceneItems.push_back(new ItemInventario(app, 668, 0, 64, 64, "wwwww", "ferrrag", app->getResources()->getImageTexture(Resources::ImagenTest)));
+	}
+}
 
 Scene::~Scene()
 {
@@ -17,6 +31,8 @@ void Scene::loadScene() {
 	//Iniciamos ite, saltamos primer ite(jugador) borrarmos el resto de items copiamos nuestra lista
 }
 
-void Scene::exitScene() {
-	//Cambia la lista actual por 
+void Scene::exitScene() { //al salir de la escena, todos los objetos de stage se vuelcan en la lista de la escena para que se queden guardados (menos el jugador)
+	SceneItems.clear();
+	SceneItems = *(app->getStateMachine()->currentState()->getStage()); //la lista stage es igual a todos los objetos de la escena
+	SceneItems.pop_front(); //quitamos al jugador de la escena (es global). Se puede hacer así o con un for que se salte el primero y copie los demás
 }
--- ProyectosSDL/HolaSDL/Scene.h
+++ ProyectosSDL/HolaSDL/Scene.h
@@ -6,10 +6,13 @@ class Scene
 public:
 	Scene();
 	~Scene();
+	Scene(SDLApp* app, int SceneNum) : app(app), SceneNum(SceneNum) {};
+	Scene(bool kk, SDLApp* app, int SceneNum);
 	void loadScene();
 	void exitScene();
 	//int getSceneNumber();
 private:
+	SDLApp* app;
 	int SceneNum;
 	list<GameObject*> SceneItems;
 	GameState* CurrentState;
