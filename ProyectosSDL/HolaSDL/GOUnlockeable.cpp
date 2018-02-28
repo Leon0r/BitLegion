e5e6871bbc2971a6faaeea9aff044be6dd6a53ab
JuBarea <jubarea@ucm.es>
2018-02-28 01:29:43 +0100

    Doors 100%?

--- ProyectosSDL/HolaSDL/GOUnlockeable.cpp
+++ ProyectosSDL/HolaSDL/GOUnlockeable.cpp
@@ -2,13 +2,16 @@
 #include "PlayState.h"
 
 void GOUnlockeable::act() {
-	if (!opened && dynamic_cast<MainCharacter*>(dynamic_cast<PlayState*>(this->getGame()->getStateMachine()->currentState())->getMainPj())->getCurrentTag() == key) {
-		opened = true;
-		secondAct();
+	if (!opened) {
+		if ( dynamic_cast<MainCharacter*>(dynamic_cast<PlayState*>(this->getGame()->getStateMachine()->currentState())->getMainPj())->getCurrentTag() == key) {
+			opened = true;
+			secondAct();
+		}
+		else
+		{
+			cout << "Cthun 2 broken" << endl;
+		}
 	}
-	else
-	{
-		cout << "Cthun to broken" << endl;
-	}
+	
 }
 
