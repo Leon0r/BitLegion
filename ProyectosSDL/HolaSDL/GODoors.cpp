4d8f130ec23c25275d28d132226badeba4d01435
L <leonorcu@ucm.es>
2018-03-06 12:58:22 +0100

    arreglado problema de pintado de puertas

--- ProyectosSDL/HolaSDL/GODoors.cpp
+++ ProyectosSDL/HolaSDL/GODoors.cpp
@@ -8,5 +8,10 @@ GODoors::~GODoors()
 }
 
 void GODoors::secondAct() {
-	app->getStateMachine()->currentState()->getStage()->push_back(new GOTransiciones(app, this->getPosition().getX(), this->getPosition().getY(), this->width_/2, this->height_/2, app->getResources()->getImageTexture(Resources::ImagenTest), scneNum));
+
+	list<GameObject*>::iterator it = app->getStateMachine()->currentState()->getStage()->begin();
+	it++; it++;
+	app->getStateMachine()->currentState()->getStage()->insert
+	( it, new GOTransiciones(app, this->getPosition().getX(), this->getPosition().getY(), this->width_ / 2, this->height_ / 2,
+		app->getResources()->getImageTexture(Resources::ImagenTest), scneNum));
 }
