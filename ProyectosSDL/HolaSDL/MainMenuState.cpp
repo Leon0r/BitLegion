5863908a044ed51459c4d3d743efb7cf84a8eaf9
Diego BV <diego.baratto8@gmail.com>
2018-04-24 12:28:40 +0200

    Cambios en el menu

--- ProyectosSDL/HolaSDL/MainMenuState.cpp
+++ ProyectosSDL/HolaSDL/MainMenuState.cpp
@@ -36,7 +36,7 @@ MainMenuState::MainMenuState(SDLApp * game):GameState(game)
 	Boton* b;
 	b = new Boton(app, "NewGame", nGame_);
 	b->setWidth(150); b->setHeight(150);
-	b->setPosition(Vector2D(app->getWindowWidth() / 2 - b->getWidth()/2, (app->getWindowHeight() - app->getWindowHeight() / 10) * 1 / 2));
+	b->setPosition(Vector2D(app->getWindowWidth() / 2 - b->getWidth() / 2, ((app->getWindowHeight() - app->getWindowHeight() / 3) * 1 / 5)+ 250));
 	b->addAnim("Feedback", { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 }, true, -1, 150);
 	b->addAnim("Stop", { 0 }, true, -1, 100);
 	btext = new AnimationRenderer(app->getResources()->getImageTexture(Resources::BotonComenzar), b->getAnimations(), 4, 5, 250, 250);
@@ -47,7 +47,7 @@ MainMenuState::MainMenuState(SDLApp * game):GameState(game)
 
 	b = (new Boton(app, "Load Game", lGame_));
 	b->setWidth(150); b->setHeight(150);
-	b->setPosition(Vector2D(app->getWindowWidth() / 2 - b->getWidth() / 2, (app->getWindowHeight() - app->getWindowHeight() / 10) * 2 / 3));
+	b->setPosition(Vector2D(app->getWindowWidth() / 2 - b->getWidth() / 2, ((app->getWindowHeight() - app->getWindowHeight() / 3) * 2 / 5)+250));
 	b->addAnim("Feedback", { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 }, true, -1, 150);
 	b->addAnim("Stop", { 0 }, true, -1, 100);
 	bLoadtext = new AnimationRenderer(app->getResources()->getImageTexture(Resources::BotonLoad), b->getAnimations(), 4, 5, 250, 250);
@@ -58,7 +58,7 @@ MainMenuState::MainMenuState(SDLApp * game):GameState(game)
 
 	b = (new Boton(app, "Exit", eGame_));
 	b->setWidth(150); b->setHeight(150);
-	b->setPosition(Vector2D(app->getWindowWidth() / 2 - b->getWidth() / 2, ((app->getWindowHeight() - app->getWindowHeight() /10) * 2 / 3) + b->getHeight()));
+	b->setPosition(Vector2D(app->getWindowWidth() / 2 - b->getWidth() / 2, ((app->getWindowHeight() - app->getWindowHeight() / 3) * 3 / 5)+250));
 	b->addAnim("Feedback", { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 }, true, -1, 150);
 	b->addAnim("Stop", { 0 }, true, -1, 100);
 	bExittext = new AnimationRenderer(app->getResources()->getImageTexture(Resources::BotonExit), b->getAnimations(), 4, 5, 250, 250);
@@ -96,8 +96,6 @@ MainMenuState::~MainMenuState()
 	delete(btext);
 	delete(bLoadtext);
 	delete(bExittext);
-	//delete(logo);
-	//delete(fondo);
 }
 
 
