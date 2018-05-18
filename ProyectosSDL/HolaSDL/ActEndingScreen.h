7fdb8ca8dc6c7302fbda757f1bf92dc1d00e9bff
JuBarea <jubarea@ucm.es>
2018-05-18 20:51:41 +0200

    Escena de transicion de actos
    Añadidas las escenas para transiciones entre actos
    
    Co-Authored-By: diegobv <diegobv@users.noreply.github.com>

---
+++ ProyectosSDL/HolaSDL/ActEndingScreen.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "GameState.h"
+#include "sdl_includes.h"
+#include "Font.h"
+//#include "Texture.h"
+
+class ActEndingScreen :
+	public GameState
+{
+public:
+	ActEndingScreen(SDLApp* app,string _header = "",float time = 5000);
+	~ActEndingScreen();
+
+	virtual void render();
+	virtual void update();
+
+	float initTime;
+	float MaxTime;
+
+	const SDL_Color col = { 0, 169, 0, 1 };
+	static int ACT;
+	std::string headers;
+	Texture fuente;
+	std::string title;
+	Font* fnt;
+	Font* fnts;
+};
+
