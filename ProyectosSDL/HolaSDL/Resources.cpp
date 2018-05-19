5d45d21d615e24c00ba81f6a57919e93aa229d7f
Unknown <celiam07@ucm.es>
2018-05-19 17:23:01 +0200

    Arreglando la cagada
    Oops

--- ProyectosSDL/HolaSDL/Resources.cpp
+++ ProyectosSDL/HolaSDL/Resources.cpp
@@ -275,8 +275,8 @@ std::vector<std::string> Resources::imageFiles_{
 "..//images/Expresiones/SeñorBazarExpresiones.png",
 "..//images/Expresiones/AmigaGalinaExpresiones.png",
 "..//images/Objetos/Takeable/IdAlena.png",
-"..//images/Expresiones/CamelloExpresiones.png",
-"..//images/Expresiones/CamelloExpresiones.png",
+"..//images/Expresiones/ExtrasActo2Expresiones.png",
+"..//images/Expresiones/ExtrasActo4Expresiones.png",
 };
 
 std::vector<std::string> Resources::musicFiles_{
