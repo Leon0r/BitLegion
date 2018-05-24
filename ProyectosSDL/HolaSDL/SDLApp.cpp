5bfe7ba6f1d49c7720d829ded341e1be956c2a82
Diego BV <diego.baratto8@gmail.com>
2018-05-24 21:22:32 +0200

    Revert "el forense te cambia de escena + niña cargando ajustada"
    This reverts commit 485e02050552b7f31608e06f95b38341b6d8e77a.

--- ProyectosSDL/HolaSDL/SDLApp.cpp
+++ ProyectosSDL/HolaSDL/SDLApp.cpp
@@ -99,10 +99,11 @@ void SDLApp::loadCarga()
 {
 	carga = new Entity(this);
 	tx = new Texture(this->getRenderer(), "..//images/SpriteSheets_Personajes/vecinaNinya.png");
-	carga->addAnim("carga", {0,1,2,3,4,5,6,7,8,9,10,11,12,13,13,12,11,10,9,8,7,6,0,1,0,1,0,0,1 }, true, -1, 200);
+	carga->addAnim("carga", { 0,3,5,6,7,8,9,10,11,12,13,14,15,16,15,14,13,12,11,
+		10,9,8,7,6,5,4,1,2,0,2,0,3,0,2,0,2,0,2,0,2,1,0,1,2,0,2,0,2,0,2,0,2 }, true, -1, 200);
 	carga->playAnim("carga");
-	carga->setHeight(81);
-	carga->setWidth(50);
+	carga->setHeight(70);
+	carga->setWidth(55);
 	carga->setPosition(Vector2D(1280-carga->getWidth()*3/2, 720-carga->getHeight()*3/2));
 	carga->addRenderComponent(new AnimationRenderer(tx, carga->getAnimations(), 4, 5, 39, 81));
 }
